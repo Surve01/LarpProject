cd70e2a3811a4028537fcd401c45e0fd5cb4706c
Akshata <Akshata@10.0.0.18>
2018-05-30 15:24:41 -0400

    initial changes

---
+++ Add_data.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <form action="add_db.php" method="POST">
+    <br>
+    Add creature name:<input type="text" name="player">
+    <br>
+    <br>
+    <div> Add Archetype  :<input type="text" name="Archetype" ></div>
+    <br>
+    <div>Add Level:<input type="text" name="Level"></div>
+    <br>
+    <div>Add Hit Points:<input type="text" name="Hit_Points"></div>
+    <br>
+    <div>Add Armor:<input type="text" name="Armor_Type"></div>
+    <br>
+    <div>Add Armor:<input type="text" name="Armor_Worn"></div>
+    <br>
+    <div>Add Weapon:<input type="text" name="Weapon_Type" ></div>
+    <br>
+    <div>Add Damage:<input type="text" name="Damage" ></div>
+    <br>
+    <p><div><textarea row="90" cols="60" placeholder="Add GM comments"></textarea></div>
+    <p><div><textarea row="90" cols="60" placeholder="Add Description"></textarea></div>
+    <p><div><textarea row="90" cols="60" placeholder="Add Roleplay"></textarea></div>
+    <p><div><textarea row="90" cols="60" placeholder= "Add Special Attack"></textarea></div>
+    <p><div><textarea row="90" cols="60" placeholder="Add Special Defence"></textarea></div>
+    <p><div><textarea row="90" cols="60" placeholder="Class Notes"></textarea></div>
+    <p><div><button type="submit" name="addCreature">Add new Creature</button></div>
+</form>
+
+
+
+</body>
+</html>
