536bc93d2df2550eb848bf5b268acfbb4e5710ad
janosky <31524403+janosky@users.noreply.github.com>
2018-08-10 13:49:36 -0400

    removed unneeded files

--- views/AddEditCreatures.php
+++ views/AddEditCreatures.php
@@ -8,111 +8,110 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <?php  
-        include "../PDO/PDOConection.php"?>
+        <?php include "../PDO/PDOConection.php" ?>
         <title>Add/Edit Creatures</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="../js/SaveEditCreature.js"></script>
     </head>
     <body>
-        
-        
+
+
         <div class ="Container">
-            
-              
+
+
             <button class="button"  id="showAdd">Add Creature</button>
             <button class="button"  id="showEdit">Edit Creature</button>
-         
-            
+
+
             <div    class="addEdit" id="addCreauture">
                 <form id='addForm' method ='post' name ='addForm' action="../PDO/SaveNewCreature.php">
-                  
-                         
-            <div  class="addEdit" id="editCreature">
-           
-                    <select   name ="creatureNameSelection" class="creatureInput" id="creatureNameDropDown">
-                     
-            
-                      </select>
-        
-                <input type="hidden" id="creatureID" value=""><
-            </div>
-                    
-                    <div id='addEditPair' class='LabeFieldPair'> 
-                      <label>Creature Name</label>         
-                    <input id='addName' class='addEditText' name='addName' type='text' required>
-                  </div>
-                    
-                  <div id='creatureTypePair' class='LabeFieldPair'> 
-                      <label>Creature Type</label>         
-                    <input id='addType' class='addEditText' name='addCreatureType' type='text' required>
-                  </div>
-                    
-                   <div  id='monsterBookPair' class='LabeFieldPair'> 
-                      <label>From Monster Book</label>         
-                    <input id='addMonsterBook' class='addEditText' type='text' required name='monsterBook'>
-                  </div>
-                  <div  id='addTerrainPair' class='LabeFieldPair'> 
-                      <label>Terrain</label>         
-                    <input id='addTerrain' class='addEditText' type='text' required name='terrain'>
-                  </div>
-                    <div  id='randomMonsterPair' class='LabeFieldPair'> 
-                      <label>Random Monster?</label>         
-                    <div id='randomMonster' class='addEditRadio' >
-                        <label>  <input type='radio' required name='randomMonster' value='yes' >Yes</label>
-                        <label>  <input type='radio' required name='randomMonster' value='no' >No</label>
 
-                        
+
+                    <div  class="addEdit" id="editCreature">
+
+                        <select   name ="creatureNameSelection" class="creatureInput" id="creatureNameDropDown">
+
+
+                        </select>
+
+                        <input type="hidden" id="creatureID" value=""><
                     </div>
-                  </div>
-                
+
+                    <div id='addEditPair' class='LabeFieldPair'> 
+                        <label>Creature Name</label>         
+                        <input id='addName' class='addEditText' name='addName' type='text' required>
+                    </div>
+
+                    <div id='creatureTypePair' class='LabeFieldPair'> 
+                        <label>Creature Type</label>         
+                        <input id='addType' class='addEditText' name='addCreatureType' type='text' required>
+                    </div>
+
+                    <div  id='monsterBookPair' class='LabeFieldPair'> 
+                        <label>From Monster Book</label>         
+                        <input id='addMonsterBook' class='addEditText' type='text' required name='monsterBook'>
+                    </div>
+                    <div  id='addTerrainPair' class='LabeFieldPair'> 
+                        <label>Terrain</label>         
+                        <input id='addTerrain' class='addEditText' type='text' required name='terrain'>
+                    </div>
+                    <div  id='randomMonsterPair' class='LabeFieldPair'> 
+                        <label>Random Monster?</label>         
+                        <div id='randomMonster' class='addEditRadio' >
+                            <label>  <input type='radio' required name='randomMonster' value='yes' >Yes</label>
+                            <label>  <input type='radio' required name='randomMonster' value='no' >No</label>
+
+
+                        </div>
+                    </div>
+
                     <div  id='frequencyPair' class='LabeFieldPair'> 
-                      <label>Frequency</label>         
-                    <input id='addFrequency' class='addEditText' type='text' required name='frequency'>
-                  </div>
-                    
+                        <label>Frequency</label>         
+                        <input id='addFrequency' class='addEditText' type='text' required name='frequency'>
+                    </div>
+
                     <div  id="descriptionPair" class='LabeFieldPair'> 
-                      <label>Description</label>         
-                      <textarea name='description' class='addEditTextarea' id='addDescription'></textarea>
-                  </div>
-                    
-                    
+                        <label>Description</label>         
+                        <textarea name='description' class='addEditTextarea' id='addDescription'></textarea>
+                    </div>
+
+
                     <div  id="backGroundPair" class='LabeFieldPair'> 
-                      <label>Background</label>         
-                      <textarea name='background' class='addEditTextarea' id='addbackground'></textarea>
-                  </div>
-                    
+                        <label>Background</label>         
+                        <textarea name='background' class='addEditTextarea' id='addbackground'></textarea>
+                    </div>
+
                     <div  id="specialAttacks" class='LabeFieldPair'> 
-                      <label>Special Attacks</label>         
-                      <textarea name='specialAttacks' class='addEditTextarea' id='addSpecialAttacks'></textarea>
-                  </div>
-                    
-                     <div  id="specialDefense" class='LabeFieldPair'> 
-                      <label>Special Defense</label>         
-                      <textarea name='specialDefense' class='addEditTextarea' id='addspecialDefense'></textarea>
-                  </div>
-                    
-                      <Button id='submitAddNew' >Submit</button>
-                <button type="reset" value="Reset">Reset</button>W
+                        <label>Special Attacks</label>         
+                        <textarea name='specialAttacks' class='addEditTextarea' id='addSpecialAttacks'></textarea>
+                    </div>
+
+                    <div  id="specialDefense" class='LabeFieldPair'> 
+                        <label>Special Defense</label>         
+                        <textarea name='specialDefense' class='addEditTextarea' id='addspecialDefense'></textarea>
+                    </div>
+
+                    <Button id='submitAddNew' >Submit</button>
+                    <button type="reset" value="Reset">Reset</button>
                 </form>
-                
-                
-           
+
+
+
             </div>
-       
-            
+
+
             <div class="addEdit" id="addEditArchetype">
-                
-               
+
+
                 <br>
                 <br>
-                
-              
-                </div>
-             
-             
+
+
+            </div>
+
+
         </div>
-        
-       
+
+
     </body>
 </html>
